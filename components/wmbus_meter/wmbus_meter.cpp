6f5a39de086c21e8a3c6210e22c8da10b619abfb
enr0c <cornelius.schwab@gmail.com>
2025-12-20 09:54:36 +0100

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch cc1101-support # Your branch is up to date with 'origin/cc1101-support'. # # Changes to be committed: #	modified:   components/wmbus_meter/wmbus_meter.cpp #	modified:   examples/esphome-watermeter-tst.yaml #	modified:   examples/working_esphome-watermeter-tst_logs.txt

--- components/wmbus_meter/wmbus_meter.cpp
+++ components/wmbus_meter/wmbus_meter.cpp
@@ -12,15 +12,34 @@ void Meter::set_meter_params(std::string id, std::string driver,
 
   this->meter = createMeter(&meter_info);
 
+  if (this->meter == nullptr) {
+    ESP_LOGE(TAG, "Failed to create meter driver '%s' for meter_id=0x%s", driver.c_str(), id.c_str());
+    ESP_LOGE(TAG, "This usually means the driver was not compiled in. Ensure wmbus_common: drivers includes '%s' (or set drivers: all).", driver.c_str());
+    this->mark_failed();
+    return;
+  }
+
   for (auto linkMode : linkModes)
     this->link_modes_.addLinkMode(linkMode);
 }
 void Meter::set_radio(wmbus_radio::Radio *radio) {
   this->radio = radio;
+  if (this->radio == nullptr) {
+    ESP_LOGE(TAG, "Radio not set");
+    this->mark_failed();
+    return;
+  }
   radio->add_frame_handler(
       [this](wmbus_radio::Frame *frame) { return this->handle_frame(frame); });
 }
 void Meter::dump_config() {
+  if (this->meter == nullptr) {
+    ESP_LOGCONFIG(TAG, "wM-Bus Meter:");
+    ESP_LOGCONFIG(TAG, "  Driver: (not initialized)");
+    ESP_LOGCONFIG(TAG, "  Status: FAILED (driver not available)");
+    return;
+  }
+
   std::string id = this->get_id();
   std::string driver = this->get_driver();
   std::string key = this->get_key();
@@ -32,20 +51,29 @@ void Meter::dump_config() {
 }
 
 std::string Meter::get_id() {
+  if (this->meter == nullptr)
+    return "unknown";
   std::vector<AddressExpression> address_expressions =
       this->meter->addressExpressions();
   return address_expressions.size() > 0 ? address_expressions[0].id : "unknown";
 }
 
-std::string Meter::get_driver() { return this->meter->driverName().str(); }
+std::string Meter::get_driver() {
+  return this->meter != nullptr ? this->meter->driverName().str() : "unknown";
+}
 
 std::string Meter::get_key() {
+  if (this->meter == nullptr)
+    return "unknown";
   MeterKeys *keys = this->meter->meterKeys();
   return keys->hasConfidentialityKey() ? bin2hex(keys->confidentiality_key)
                                        : "not-encrypted";
 }
 
 void Meter::handle_frame(wmbus_radio::Frame *frame) {
+  if (this->is_failed() || this->meter == nullptr)
+    return;
+
   if (!this->link_modes_.has(frame->link_mode())) {
     ESP_LOGW(TAG, "Frame link mode %s not supported by meter %s",
              toString(frame->link_mode()), this->meter->name().c_str());
@@ -74,6 +102,8 @@ void Meter::handle_frame(wmbus_radio::Frame *frame) {
 }
 
 std::string Meter::as_json(bool pretty_print) {
+  if (this->meter == nullptr || this->last_telegram == nullptr)
+    return "{}";
   std::string json;
   this->meter->printMeter(this->last_telegram.get(), nullptr, nullptr, '\t',
                           &json, nullptr, nullptr, nullptr, pretty_print);
@@ -81,6 +111,9 @@ std::string Meter::as_json(bool pretty_print) {
 }
 
 optional<std::string> Meter::get_string_field(std::string field_name) {
+
+  if (this->meter == nullptr)
+    return {};
 
   if (field_name == "timestamp")
     return this->meter->datetimeOfUpdateHumanReadable();
@@ -97,8 +130,14 @@ optional<std::string> Meter::get_string_field(std::string field_name) {
 
 optional<float> Meter::get_numeric_field(std::string field_name) {
   // RSSI is not handled by meter but by telegram :/
-  if (field_name == "rssi_dbm")
+  if (field_name == "rssi_dbm") {
+    if (this->last_telegram == nullptr)
+      return {};
     return this->last_telegram->about.rssi_dbm;
+  }
+
+  if (this->meter == nullptr)
+    return {};
 
   if (field_name == "timestamp")
     return this->meter->timestampLastUpdate();
